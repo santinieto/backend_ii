241703bed086c9a8dc6fe80f316ae774ec92461d
Santiago Enrique Nieto <santi.nieto@live.com>
2025-04-12 02:47:04 -0300

    Aplico el CustomRouter al enrutador de autenticacion

--- src/routes/api/auth.router.js
+++ src/routes/api/auth.router.js
@@ -1,108 +1,73 @@
-import { response, Router } from "express";
+import CustomRouter from "../custom.router.js";
 import passportCallback from "../../middlewares/passport_callback.mid.js"; // Importamos el middleware de passport
 
-const authRouter = Router();
-
 const register = async (req, res, next) => {
-    try {
-        res.status(201).json({
-            response: req.user._id, // Respondemos solo con el ID del usuario registrado
-            method: req.method,
-            url: req.originalUrl,
-            status: "success",
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.status(201).json({
+        response: req.user._id, // Respondemos solo con el ID del usuario registrado
+        method: req.method,
+        url: req.originalUrl,
+        status: "success",
+    });
 };
 
 const login = async (req, res, next) => {
-    try {
-        res.cookie("token", req.token, {
-            maxAge: 1000 * 60 * 60, // 1 hora
-            httpOnly: true, // Solo el servidor puede acceder a la cookie
-        })
-            .status(200)
-            .json({
-                response: req.token, // Respondemos con el usuario de acceso
-                method: req.method,
-                url: req.originalUrl,
-                status: "success",
-            });
-    } catch (error) {
-        next(error);
-    }
+    res.cookie("token", req.token, {
+        maxAge: 1000 * 60 * 60, // 1 hora
+        httpOnly: true, // Solo el servidor puede acceder a la cookie
+    })
+        .status(200)
+        .json({
+            response: req.token, // Respondemos con el usuario de acceso
+            method: req.method,
+            url: req.originalUrl,
+            status: "success",
+        });
 };
 
 const logout = async (req, res, next) => {
-    try {
-        res.clearCookie("token").status(200).json({
-            response: "Logout exitoso!",
-            method: req.method,
-            url: req.originalUrl,
-            status: "success",
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.clearCookie("token").status(200).json({
+        response: "Logout exitoso!",
+        method: req.method,
+        url: req.originalUrl,
+        status: "success",
+    });
 };
 
 const profile = async (req, res, next) => {
-    try {
-        const user = req.user;
-        res.status(200).json({
-            response: user,
-            method: req.method,
-            url: req.originalUrl,
-            status: "success",
-        });
-    } catch (error) {
-        next(error);
-    }
+    const user = req.user;
+    res.status(200).json({
+        response: user,
+        method: req.method,
+        url: req.originalUrl,
+        status: "success",
+    });
 };
 
 const controlPanel = async (req, res, next) => {
-    try {
-        const user = req.user;
-        res.status(200).json({
-            response: user,
-            method: req.method,
-            url: req.originalUrl,
-            status: "success",
-        });
-    } catch (error) {
-        next(error);
-    }
+    const user = req.user;
+    res.status(200).json({
+        response: user,
+        method: req.method,
+        url: req.originalUrl,
+        status: "success",
+    });
 };
 
-authRouter.post(
-    "/register",
-    passportCallback("register"), // Implementamos la estrategia de registro
-    register
-);
+class AuthRouter extends CustomRouter {
+    constructor() {
+        super();
+        this.init();
+    }
 
-authRouter.post(
-    "/login",
-    passportCallback("login"), // Implementamos la estrategia de login
-    login
-);
+    init = () => {
+        this.create("/register", passportCallback("register"), register);
+        this.create("/login", passportCallback("login"), login);
+        this.read("/profile", passportCallback("current"), profile);
+        this.read("/logout", passportCallback("current"), logout);
+        this.read("/control-panel", passportCallback("admin"), controlPanel);
+    };
+}
 
-authRouter.get(
-    "/profile",
-    passportCallback("current"), // Implementamos la estrategia de JWT
-    profile
-);
-
-authRouter.get(
-    "/logout",
-    passportCallback("current"), // Implementamos la estrategia de JWT
-    logout
-);
-
-authRouter.get(
-    "/control-panel",
-    passportCallback("admin"), // Implementamos la estrategia de JWT
-    controlPanel
-);
+const authRouter = new AuthRouter().getRouter();
 
 export default authRouter;
